9889e8f36ef7ab21e7bc9eb01ecaa82969a1dac3
goyalxatul <atulxgoyal@gmail.com>
2024-10-29 21:50:48 +0530

    changes

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -9,12 +9,15 @@ const Contact = () => {
       </div>
 
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
-        {/* Removed the image and asset reference */}
-        <div className='flex flex-col justify-center items-start gap-6'>
+        {/* Card for Office Information */}
+        <div className='bg-white shadow-lg rounded-lg p-6 flex flex-col justify-center items-start gap-6 w-full md:w-1/2'>
           <p className='font-semibold text-xl text-gray-600'>Our Office</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className=' text-gray-500'>Tel: +91 - 57484939033<br /> Email: contact@yourcompany.com</p>
-         
+          <p className='text-gray-500'>
+            Symboisis International University <br /> Lavale ,Pune
+          </p>
+          <p className='text-gray-500'>
+            Tel: +91-9991268863<br /> Email: manager@caringnannies.com
+          </p>
         </div>
       </div>
     </div>
